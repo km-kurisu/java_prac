cc552ffd598f38b6ec03bfab020bed177bf8c450
km-kurisu <kamleshkmistry33@gmail.com>
2023-11-17 23:54:43 +0530

    modified:   Prac_1.java 	modified:   Prac_2.java

--- Prac_1.java
+++ Prac_1.java
@@ -1,32 +1,26 @@
 //Q1. write a program to demonstrate textbox, radio button and checkbox.
 
-//importing applet, awt and swings
+
 import java.applet.*;
 import java.awt.*;
 import javax.swing.*;
 
 public class Prac_1 extends Applet{
     
-    //you can use public static void main it works as well
     public void init(){
         
-        //text area
-        TextArea t1 = new TextArea("This is a TextArea");
+        TextArea t1 = new TextArea("This is a TextArea");   
         
-        //textfield
         TextField Tf1 = new TextField("this is a text field");
-
-        //radiobutton uses JRadioButtonMenuItem from the swings class
+    
         JRadioButtonMenuItem r1 = new JRadioButtonMenuItem("Pick one");
         JRadioButtonMenuItem r2 = new JRadioButtonMenuItem("Pick two");
         JRadioButtonMenuItem r3 = new JRadioButtonMenuItem("Pick three");
 
-        //checkbox
         Checkbox cb1 = new Checkbox("This is checkbox One");
         Checkbox cb2 = new Checkbox("This is checkbox Two");
         Checkbox cb3 = new Checkbox("This is checkbox Three");
         
-        //adding all the components 
         add(Tf1);
         add(t1);
         add(r1);
--- Prac_2.java
+++ Prac_2.java
@@ -1,2 +1,28 @@
 //Q2. Write a program in java to create choices and lists of various cities in india
 
+import java.applet.*;
+import java.awt.*;
+
+public class Prac_2 extends Applet{
+    public void init(){
+        List l1 = new List( 4);
+        l1.add("Mumbai");
+        l1.add("chennai");
+        l1.add("Delhi");
+        
+
+        Choice ch1 = new Choice();
+        ch1.add("patna");
+        ch1.add("indore");
+        ch1.add("kanpur");
+
+        add(l1);
+        add(ch1);
+    }
+}
+
+
+/*<APPLET CODE="Prac_2.class" 
+    HEIGHT=600
+    WIDTH=600>
+</APPLET> */
